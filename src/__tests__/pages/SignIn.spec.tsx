976bfc6ec35e3dd62f961222c73d6fca43c6f3cb
lulygomes <luiz.gsilva42@gmai.com>
2020-12-17 10:04:23 -0300

    First test

---
+++ src/__tests__/pages/SignIn.spec.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render } from '@testing-library/react'
+import SingIn from '../../pages/SignIn'
+
+jest.mock('react-router-dom', () => {
+  return {
+    useHistory: jest.fn(),
+    Link: ({ children }: { children: React.ReactNode}) => children,
+  }
+})
+
+describe('SingIn Pge', () => {
+  it('should be able to sign in', () => {
+    const { debug } = render(<SingIn />);
+
+    debug();
+  })
+})
